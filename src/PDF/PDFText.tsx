8aacbac2360b2a85672dfc70a26f1fef8aa2c43d
Samuel Sylvester <sam-g-steel@users.noreply.github.com>
2019-01-26 03:49:22 -0600

    Started On The DQM header

--- src/PDF/PDFText.tsx
+++ src/PDF/PDFText.tsx
@@ -10,16 +10,20 @@ interface PDFTextProps {
 
 export class PDFText extends React.Component<PDFTextProps> {
     static defaultProps = {
+        bold: false,
         fontSize: 8,
+        italic: false,
         text: "",
     };
 
-    props: any & PDFPageProps;
+    props: any & PDFTextProps;
     render() {
         return (
             <span
                 style={{
                     fontSize: pointsToInches(this.props.fontSize) * 3 + "rem",
+                    fontStyle: this.props.italic ? "italic" : "normal",
+                    fontWeight: this.props.bold ? "bold" : "normal",
                 }}
             >
                 {this.props.text}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { PDFText } from "./PDF/PDFText";
 
 import "./styles.css";
 
+import { DQMLogo } from "./DQMLogo";
+
 const { flip } = PaperSize;
 
 function App() {
@@ -13,7 +15,31 @@ function App() {
             <br />
             <b>Hello CodeSandbox</b>
             <PDFPage pageSize={flip(PaperSize.Letter)}>
-                <PDFText fontSize={12} text="Dredging Plot" />
+                {/* Header */}
+                <div style={{ display: "flex" }}>
+                    <div style={{ flex: 0 }}>
+                        <PDFText bold italic fontSize={8} text="DREDGING" />
+                        <br />
+                        <PDFText bold italic fontSize={8} text="QUALITY" />
+                        <br />
+                        <PDFText bold italic fontSize={8} text="MANAGEMENT" />
+                    </div>
+                    <div style={{ maxWidth: "32%" }}>
+                        <img src={DQMLogo} style={{ height: "33%" }} />
+                    </div>
+                    <div style={{ flex: 1 }}>
+                        <PDFText bold fontSize={12} text="Cumulative Dredging Plot" />
+                        <br />
+                        <br />
+                        <PDFText fontSize={12} text="Plant: Columba " />
+                        <PDFText fontSize={12} italic text="(Hopper)" />
+                        <br />
+                        <PDFText fontSize={12} text="1 Day Tracking 7/12/2018 - 7/13/2018" />
+                    </div>
+                    <div style={{ flex: 0.5 }}>
+                        <PDFText bold fontSize={12} text="Dredging Plot" />
+                    </div>
+                </div>
             </PDFPage>
         </div>
     );
